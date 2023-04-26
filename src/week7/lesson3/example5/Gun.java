7f18b62cb20f7dfc2a251a36d45939907b93b953
ilyas <zhuanyshev.ilyas@gmail.com>
2023-04-27 01:49:22 +0600

    Constructor

--- src/week7/lesson3/example5/Gun.java
+++ src/week7/lesson3/example5/Gun.java
@@ -13,7 +13,7 @@ public class Gun {
     }
 
     public Gun(String name, int bulletsCapacity, int currentBullets){
-        this.name = name;
+        this.name = name; //this class' name is equal to our temporary name
         this.bulletsCapacity = bulletsCapacity;
         this.currentBullets = currentBullets;
     }
@@ -22,6 +22,27 @@ public class Gun {
         System.out.println("..........................");
         System.out.println("Gun " + this.name + ", capacity: " + this.bulletsCapacity + " bullets");
         System.out.println("Current bullet amount: " + this.currentBullets + " bullets");
+    }
+
+    public void shot() {
+        System.out.println("..........................");
+        if (this.currentBullets > 0) {
+            System.out.println("Baaaam!");
+            this.currentBullets = this.currentBullets - 1;
+        }else{
+            System.out.println("Shell is empty!");
+        }
+    }
+
+    public void reload() {
+        System.out.println("..........................");
+        if (this.currentBullets < this.bulletsCapacity) {
+            this.currentBullets = this.currentBullets + 1;
+            System.out.println("Reloading... Added 1 bullet");
+            System.out.println("Currently " + this.currentBullets + " bullets");
+        } else {
+            System.out.println("Shell is full");
+        }
     }
 
 }
--- src/week7/lesson3/example5/Main.java
+++ src/week7/lesson3/example5/Main.java
@@ -1,8 +1,38 @@
 package week7.lesson3.example5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Gun makarov = new Gun("Makarov's pistol", 7, 5);
-        makarov.printStatus();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("-------- CS GO GAME BY ILYAS ---------");
+        Gun pistol = new Gun("Colt 17", 7, 0);
+
+        while(true){
+
+            System.out.println("----------------------------------");
+            System.out.println("PRESS [1] TO RELOAD GUN");
+            System.out.println("PRESS [2] TO SHOT WITH GUN");
+            System.out.println("PRESS [3] TO CHECK STATUS");
+            System.out.println("PRESS [0] TO EXIT");
+
+            String choice = in.next();
+
+            if(choice.equals("0")){
+                break;
+            }else if(choice.equals("1")){
+                pistol.reload();
+            }else if(choice.equals("2")){
+                pistol.shot();
+            }else if(choice.equals("3")){
+                pistol.printStatus();
+            }else{
+                System.out.println("Wrong Command!");
+            }
+
+        }
+
     }
 }
