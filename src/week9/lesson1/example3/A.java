4185981e1bf49354fbeb9658f3f3ece498551b19
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-09 00:39:42 +0600

    Abstract

---
+++ src/week9/lesson1/example3/A.java
@@ -0,0 +1,6 @@
+package week9.lesson1.example3;
+
+public abstract class A {
+    public abstract int x();
+
+}
---
+++ src/week9/lesson1/example3/B.java
@@ -0,0 +1,8 @@
+package week9.lesson1.example3;
+
+public class B extends A{
+    @Override
+    public int x() {
+        return 0;
+    }
+}
