bcc3f7d4b012d02056f6455d50ef6d2c2b58511a
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-11 00:22:03 +0600

    Interfaces

---
+++ src/week9/lesson3/example2/B2Spirit.java
@@ -0,0 +1,44 @@
+package week9.lesson3.example2;
+
+public class B2Spirit implements Aircraft, MilitaryTechnique{
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void turnLeft() {
+
+    }
+
+    @Override
+    public void turnRight() {
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void scan() {
+
+    }
+
+    @Override
+    public void reload() {
+
+    }
+}
---
+++ src/week9/lesson3/example2/MilitaryTechnique.java
@@ -0,0 +1,7 @@
+package week9.lesson3.example2;
+
+public interface MilitaryTechnique {
+    void attack();
+    void scan();
+    void reload();
+}
