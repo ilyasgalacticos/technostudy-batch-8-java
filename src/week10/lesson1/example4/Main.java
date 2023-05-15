7f2f30a3eecad27196f351d6d29bbb729a4a5aea
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-16 00:12:23 +0600

    TRY CATCH

---
+++ src/week10/lesson1/example4/Main.java
@@ -0,0 +1,12 @@
+package week10.lesson1.example4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        System.out.println(a/b);
+        int x = 10;
+        int y = 20;
+        System.out.println(a + b);
+    }
+}
