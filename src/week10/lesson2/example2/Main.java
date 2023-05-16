3a06ba42139236aa0a9a7f7c7de9146c1ec13a52
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-16 23:39:08 +0600

    TRY CATCH

--- src/week10/lesson2/example2/Main.java
+++ src/week10/lesson2/example2/Main.java
@@ -1,4 +1,23 @@
 package week10.lesson2.example2;
 
+import java.util.InputMismatchException;
+
 public class Main {
+    public static void main(String[] args) {
+        try{
+            int arr[] = {10,20};
+            System.out.println(arr[0]);
+            System.out.println("YES");
+        }catch (InputMismatchException e){
+            System.out.println(e.getMessage());
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("Finally I am done");
+        }
+    }
 }
