3e249768c0adf0faa3f59aed1d059727eb826767
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-25 00:13:02 +0600

    SETS

---
+++ src/week11/lesson4/example1/Main.java
@@ -0,0 +1,10 @@
+package week11.lesson4.example1;
+
+public class Main {
+    public static void main(String[] args) {
+        WeekDay day = WeekDay.THURSDAY;
+        if (day == WeekDay.THURSDAY) {
+            System.out.println("Today is thursday");
+        }
+    }
+}
---
+++ src/week11/lesson4/example1/WeekDay.java
@@ -0,0 +1,11 @@
+package week11.lesson4.example1;
+
+public enum WeekDay {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
