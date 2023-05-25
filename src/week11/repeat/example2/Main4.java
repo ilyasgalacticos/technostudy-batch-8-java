d38bbe3dd29143e8d3ed5420c033fd754d906401
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-26 00:55:16 +0600

    SETS

--- src/week11/repeat/example2/Main4.java
+++ src/week11/repeat/example2/Main4.java
@@ -2,6 +2,7 @@ package week11.repeat.example2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Main4 {
     public static void main(String[] args) {
@@ -13,11 +14,18 @@ public class Main4 {
         exams.put("zhazira", 88);
         exams.put("ali", 89);
         exams.put("john", 75);
+        exams.put("cankut", 75);
 
         System.out.println(exams);
+
         System.out.println(exams.get("ali"));
         System.out.println(exams.get("ilyas"));
         System.out.println(exams.get("sarah"));
 
+        Set<String> names = exams.keySet();
+
+        for(String key : names){
+            System.out.println(key + " got " + exams.get(key) + " points");
+        }
     }
 }
