643dc5844832c6d90afae1a2c576a512866b3a18
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-26 01:23:04 +0600

    SETS

---
+++ src/week11/files/Example1.java
@@ -0,0 +1,32 @@
+package week11.files;
+
+import java.io.*;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("input.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String text;
+            while ((text = bufferedReader.readLine()) != null) { // nextLine();
+                System.out.println(text);
+            }
+            bufferedReader.close();
+            fileReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
