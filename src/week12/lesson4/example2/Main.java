da8f6f123a27757371856671e3138770270547c0
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-31 01:52:43 +0600

    String Builder

--- src/week12/lesson4/example2/Main.java
+++ src/week12/lesson4/example2/Main.java
@@ -1,4 +1,28 @@
 package week12.lesson4.example2;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 60; i++) {
+            for (int j = 0; j < 60; j++) {
+                String min = String.valueOf(i);
+                String sec = String.valueOf(j);
+                if (i < 10) {
+                    min = "0" + min;
+                }
+                if (j < 10) {
+                    sec = "0" + sec;
+                }
+                try{
+                    Thread.sleep(500);
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println(min + " : " + sec);
+                }catch (Exception e){
+                }
+            }
+        }
+    }
+
 }
