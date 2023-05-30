01990d867944f46a4b9c14b227863ba8862365fb
ilyas <zhuanyshev.ilyas@gmail.com>
2023-05-31 00:31:11 +0600

    String Buffer

--- src/week12/lesson3/example1/Main4.java
+++ src/week12/lesson3/example1/Main4.java
@@ -4,8 +4,12 @@ public class Main4 {
     public static void main(String[] args) {
 
         StringBuffer stringBuffer = new StringBuffer("Java Enterprise Edition");
-        char[] array = new char[10];
-        stringBuffer.getChars(5, 15, array, 0);
+        char[] array = new char[30];
+        array[0] = 'M';
+        array[1] = 'y';
+        array[2] = ' ';
+
+        stringBuffer.getChars(5, 15, array, 3);
         for(char c : array){
             System.out.println(c);
         }
