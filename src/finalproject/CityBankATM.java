6c8c78588bfb5f8a3c65fc8860eda88794c06172
ilyas <zhuanyshev.ilyas@gmail.com>
2023-06-01 23:50:16 +0600

    FINAL PROJECT

--- src/finalproject/CityBankATM.java
+++ src/finalproject/CityBankATM.java
@@ -1,6 +1,24 @@
 package finalproject;
 
+import java.util.Scanner;
+
 public class CityBankATM {
 
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+
+            System.out.println("INSERT ACCOUNT NUMBER:");
+            String account = in.next();
+            System.out.println("INSERT PIN CODE:");
+            String pinCode = in.next();
+
+            
+
+        }
+
+    }
 
 }
--- src/finalproject/Database.java
+++ src/finalproject/Database.java
@@ -20,4 +20,5 @@ public class Database {
     public static ArrayList<BankAccount> getAccounts(){
         return accounts;
     }
+
 }
