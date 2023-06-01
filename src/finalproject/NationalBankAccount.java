42531fbc209bbfd0bb1d3d0def57c82c12e515e7
ilyas <zhuanyshev.ilyas@gmail.com>
2023-06-01 23:38:27 +0600

    FINAL PROJECT

--- src/finalproject/NationalBankAccount.java
+++ src/finalproject/NationalBankAccount.java
@@ -7,6 +7,17 @@ public class NationalBankAccount implements BankAccount{
     private String accountNumber;
     private String pinCode;
 
+    public NationalBankAccount(String fullName, int balance, String accountNumber, String pinCode) {
+        this.fullName = fullName;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.pinCode = pinCode;
+    }
+
+    public NationalBankAccount(){
+
+    }
+
     @Override
     public String getAccountNumber() {
         return this.accountNumber;
@@ -39,6 +50,26 @@ public class NationalBankAccount implements BankAccount{
     @Override
     public void debitBalance(int debit) {
         this.balance = this.balance + debit;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
     }
 
     @Override
