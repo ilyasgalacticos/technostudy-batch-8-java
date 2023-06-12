6ff1977fbf5469504bdfd57b5a6265452aa260b4
ilyas <zhuanyshev.ilyas@gmail.com>
2023-06-12 23:26:33 +0600

    LIBRARY PROJECT

--- src/libraryproject/Library.java
+++ src/libraryproject/Library.java
@@ -13,16 +13,21 @@ public class Library implements LibraryOperations {
 
     @Override
     public void addBook(Book b) {
-
+        books.add(b);
     }
 
     @Override
     public void removeBook(Book b) {
-
+        books.remove(b);
     }
 
     @Override
     public Book searchBookByTitle(String title) {
+        for(Book book : books){
+            if(book.getTitle().equals(title)){
+                return book;
+            }
+        }
         return null;
     }
 
